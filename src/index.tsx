9aa69fd40369c134088407c21df152212f09af98
IVLIU <IVLIU.github.com>
2018-04-02 15:53:38 +0800

    .cache

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Button } from 'antd';
+
+ReactDOM.render((
+    <Button>Hello World</Button>
+), document.querySelector('#root'));
